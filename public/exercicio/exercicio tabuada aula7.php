5d6f2a36cac68b8a694d5bf38bd23fd5d8ae4d49
272807-coder <272807@unifio.edu.br>
2025-10-07 22:48:39 +0000

    commit 0

---
+++ public/exercicio/exercicio tabuada aula7.php
@@ -0,0 +1,12 @@
+<h1>tabuada</h1>
+
+<form action="for.php" method="post">
+
+<Label for="number">Digite um numero:</Label>
+<input type="number" id="number" number="number">
+<input type="submit" value="gerar tabuada">
+
+
+
+
+</form>
